d85a49e786a5611c5f02b67f8d66bc31a317ce53
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2025-03-02 16:21:00 +0530

    Update chat.jsx

--- src/chatbot/chat.jsx
+++ src/chatbot/chat.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://thinkbot-ggwc.onrender.com");
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
